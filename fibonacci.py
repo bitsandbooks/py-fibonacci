b7f42af5a9e10e35937f0da1f5cf5c4903e0e8eb
Rob Dumas <rob@bitsandbooks.com>
2012-05-15 18:40:05 -0500

    Part 1: A simple Fibonacci sequencer.

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,47 @@
 #!/usr/bin/python
 """
-fibonacci.py: A simple Fibonacci sequencer.
+In order to print a Fibonacci sequence, we need to tell Python what to do. 
 
-In mathematical terms, the sequence F(n) of Fibonacci numbers is defined by 
-adding the two previous numbers in the sequence, like so:
-  F(n) = F(n-1) + F(n-2),
+With just a few commands, Python will print out the first twelve Fibonacci 
+numbers for us.
 
-Let's start with 0 and 1 as our "seed" values and create a list of the first 
-twelve Fibonacci numbers. Using pen and paper, we get the following sequence:
-  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+Note: Any text appearing between triple quotes (like this block of text) is a 
+BLOCK COMMENT, while any text that appears between a pound sign (#) and the end 
+of the line is an INLINE COMMENT. Python will ignore them.
+"""
+
+# Set our seed values of 0 and 1.
+f0, f1 = 0, 1 
+
+# Since f0 and f1 are the first two numbers in out sequence, 
+# let's print them out. Two down, ten to go!
+print f0
+print f1
+
+# This is a FOR LOOP, which will repeat as long as its condition (i) is true. 
+# Each time we reach the end of the loop, i is incremented. When the condition 
+# is no longer true (i.e., when i is not in the range specified), we exit the 
+# loop. (Note: our range starts at 2, because we already printed the first two 
+# numbers above.
+for i in range(2,12): 
+  fn = f1 + f0 # Here's our Fibonacci formula.
+  print fn
+  f0 = f1 # Set f0 to be whatever f1 is. (f1 is not changed.)
+  f1 = fn
+
+"""
+Result:
+$ ~: fib.py
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
 """
--- readme.md
+++ readme.md
@@ -1,8 +1,14 @@
 # Using Python to Print Fibonacci Sequences
 
-## About this repository
+## About this Repository
 
 This repository is an educational tool. It has been written to introduce the reader to some of Python's basic concepts. It is also an experiment in how Git can be used to walk a reader through the creation and evolution of a simple program. 
+
+## Using this Repository
+
+Start from the introductory commit (bitsandbooks/py-fibonacci@f938ae1e92cbbcc81746187cad161f913c66a885) and read the comments at the top of *fibonacci.py* to progress.
+
+## Additional Notes
 
 This repository is made available under a [Creative Commons Attribution-NonCommercial-ShareAlike 3.0](http://creativecommons.org/licenses/by-nc-sa/3.0/) license.
 
