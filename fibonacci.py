f938ae1e92cbbcc81746187cad161f913c66a885
Rob Dumas <rob@bitsandbooks.com>
2012-05-15 18:13:35 -0500

    Introduction. States the issue to be solved.

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python
 """
 fibonacci.py: A simple Fibonacci sequencer.
+
+In mathematical terms, the sequence F(n) of Fibonacci numbers is defined by 
+adding the two previous numbers in the sequence, like so:
+  F(n) = F(n-1) + F(n-2),
+
+Let's start with 0 and 1 as our "seed" values and create a list of the first 
+twelve Fibonacci numbers. Using pen and paper, we get the following sequence:
+  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
 """
